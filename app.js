3363a0a7ca36fc94e132134a62625e7d7b9f19fc
Ichwanul Muslim <82445431+WeakBee@users.noreply.github.com>
2022-06-23 08:33:14 +0700

    update

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
-let show1=document.getElementsByClassName("show1");
-let show2=document.getElementsByClassName("show2");
-let show3=document.getElementsByClassName("show3");
-let show4=document.getElementsByClassName("show4");
-let show5=document.getElementsByClassName("show5");
-let show6=document.getElementsByClassName("show6");
-
-function hapussemua(){
-    for(element of show1){
-        element.classList.remove("show1")
-    }
-    for(element of show2){
-      element.classList.remove("show2")
-    }
-    for(element of show3){
-      element.classList.remove("show3")
-    }
-    for(element of show4){
-      element.classList.remove("show4")
-    }
-    for(element of show5){
-      element.classList.remove("show5")
-    }
-    for(element of show6){
-      element.classList.remove("show6")
-    }
+let numb = document.querySelector(".swiper-container").children.length;
+let getShow = [];
+for (i=1; i<= numb; i++){
+  getShow[i] = document.getElementsByClassName("show"+i);
 }
 
-let numb = document.querySelector(".swiper-container").children.length;
+function hapussemua(){
+  for (i=1; i<= numb; i++){
+    for(element of getShow[i]){
+        element.classList.remove("show"+i)
+    }
+  }
+}
+
 let swiperSlide = [];
 let ss = [];
 let show = [];
@@ -36,35 +21,161 @@ for (i = 1; i <= numb;i++){
   swiperSlide[i] = document.querySelector(ss[i]);
 }
 
+let canGo = true;
+let delay = 600;
+
+let delayleft = function () {
+  if (canGo) {
+      canGo = false;
+      // do whatever you want
+      fungsileft();
+      setTimeout(function () {
+          canGo = true;
+      }, delay)
+  }
+}
+
+function fungsileft(){
+  if (swiperSlide[1].classList.contains(show[1])){
+    hapussemua();
+    swiperSlide[1].classList.add(show[6]);
+    swiperSlide[2].classList.add(show[1]);
+    swiperSlide[3].classList.add(show[2]);
+    swiperSlide[4].classList.add(show[3]);
+    swiperSlide[5].classList.add(show[4]);
+    swiperSlide[6].classList.add(show[5]);
+  } else if (swiperSlide[1].classList.contains(show[2])){
+    hapussemua();
+    swiperSlide[1].classList.add(show[1]);
+    swiperSlide[2].classList.add(show[2]);
+    swiperSlide[3].classList.add(show[3]);
+    swiperSlide[4].classList.add(show[4]);
+    swiperSlide[5].classList.add(show[5]);
+    swiperSlide[6].classList.add(show[6]);
+  } else if (swiperSlide[1].classList.contains(show[3])){
+    hapussemua();
+    swiperSlide[1].classList.add(show[2]);
+    swiperSlide[2].classList.add(show[3]);
+    swiperSlide[3].classList.add(show[4]);
+    swiperSlide[4].classList.add(show[5]);
+    swiperSlide[5].classList.add(show[6]);
+    swiperSlide[6].classList.add(show[1]);
+  } else if (swiperSlide[1].classList.contains(show[4])){
+    hapussemua();
+    swiperSlide[1].classList.add(show[3]);
+    swiperSlide[2].classList.add(show[4]);
+    swiperSlide[3].classList.add(show[5]);
+    swiperSlide[4].classList.add(show[6]);
+    swiperSlide[5].classList.add(show[1]);
+    swiperSlide[6].classList.add(show[2]);
+  } else if (swiperSlide[1].classList.contains(show[5])){
+    hapussemua();
+    swiperSlide[1].classList.add(show[4]);
+    swiperSlide[2].classList.add(show[5]);
+    swiperSlide[3].classList.add(show[6]);
+    swiperSlide[4].classList.add(show[1]);
+    swiperSlide[5].classList.add(show[2]);
+    swiperSlide[6].classList.add(show[3]);
+  } else if (swiperSlide[1].classList.contains(show[6])){
+    hapussemua();
+    swiperSlide[1].classList.add(show[5]);
+    swiperSlide[2].classList.add(show[6]);
+    swiperSlide[3].classList.add(show[1]);
+    swiperSlide[4].classList.add(show[2]);
+    swiperSlide[5].classList.add(show[3]);
+    swiperSlide[6].classList.add(show[4]);
+  } 
+}
+
 function left() {
-    if (swiperSlide[1].classList.contains(show[1])){
-        for (i = 1;i <= numb; i++){
-            if(i == 1){
-                swiperSlide[i].classList.add(show[numb]);
-            } else{
-                swiperSlide[i].classList.add(show[i-1]);
-            }
-        }
-        hapussemua();
-    } else if (swiperSlide[1].classList.contains(show[2])){
-        for (i = 1;i <= numb; i++){
-
-        }
-    } else if (swiperSlide[1].classList.contains(show[3])){
-        for (i = 1;i <= numb; i++){
-
-        }
-    } else if (swiperSlide[1].classList.contains(show[4])){
-        for (i = 1;i <= numb; i++){
-
-        }
-    } else if (swiperSlide[1].classList.contains(show[5])){
-        for (i = 1;i <= numb; i++){
-
-        }
-    } else if (swiperSlide[1].classList.contains(show[6])){
-        for (i = 1;i <= numb; i++){
-
-        }
-    }
+  delayleft();
 };
+
+let delayright = function () {
+  if (canGo) {
+      canGo = false;
+      // do whatever you want
+      fungsileft();
+      setTimeout(function () {
+          canGo = true;
+      }, delay)
+  }
+}
+
+function fungsiright(){
+  if (swiperSlide[1].classList.contains(show[1])){
+    hapussemua();
+    swiperSlide[1].classList.add(show[2]);
+    swiperSlide[2].classList.add(show[3]);
+    swiperSlide[3].classList.add(show[4]);
+    swiperSlide[4].classList.add(show[5]);
+    swiperSlide[5].classList.add(show[6]);
+    swiperSlide[6].classList.add(show[1]);
+  } else if (swiperSlide[1].classList.contains(show[2])){
+    hapussemua();
+    swiperSlide[1].classList.add(show[3]);
+    swiperSlide[2].classList.add(show[4]);
+    swiperSlide[3].classList.add(show[5]);
+    swiperSlide[4].classList.add(show[6]);
+    swiperSlide[5].classList.add(show[1]);
+    swiperSlide[6].classList.add(show[2]);
+  } else if (swiperSlide[1].classList.contains(show[3])){
+    hapussemua();
+    swiperSlide[1].classList.add(show[4]);
+    swiperSlide[2].classList.add(show[5]);
+    swiperSlide[3].classList.add(show[6]);
+    swiperSlide[4].classList.add(show[1]);
+    swiperSlide[5].classList.add(show[2]);
+    swiperSlide[6].classList.add(show[3]);
+  } else if (swiperSlide[1].classList.contains(show[4])){
+    hapussemua();
+    swiperSlide[1].classList.add(show[5]);
+    swiperSlide[2].classList.add(show[6]);
+    swiperSlide[3].classList.add(show[1]);
+    swiperSlide[4].classList.add(show[2]);
+    swiperSlide[5].classList.add(show[3]);
+    swiperSlide[6].classList.add(show[4]);
+  } else if (swiperSlide[1].classList.contains(show[5])){
+    hapussemua();
+    swiperSlide[1].classList.add(show[6]);
+    swiperSlide[2].classList.add(show[1]);
+    swiperSlide[3].classList.add(show[2]);
+    swiperSlide[4].classList.add(show[3]);
+    swiperSlide[5].classList.add(show[4]);
+    swiperSlide[6].classList.add(show[5]);
+  } else if (swiperSlide[1].classList.contains(show[6])){
+    hapussemua();
+    swiperSlide[1].classList.add(show[1]);
+    swiperSlide[2].classList.add(show[2]);
+    swiperSlide[3].classList.add(show[3]);
+    swiperSlide[4].classList.add(show[4]);
+    swiperSlide[5].classList.add(show[5]);
+    swiperSlide[6].classList.add(show[6]);
+  } 
+}
+
+function right() {
+  delayright();
+};
+
+// else if (swiperSlide[1].classList.contains(show[2])){
+//   for (i = 1;i <= numb; i++){
+
+//   }
+// } else if (swiperSlide[1].classList.contains(show[3])){
+//   for (i = 1;i <= numb; i++){
+
+//   }
+// } else if (swiperSlide[1].classList.contains(show[4])){
+//   for (i = 1;i <= numb; i++){
+
+//   }
+// } else if (swiperSlide[1].classList.contains(show[5])){
+//   for (i = 1;i <= numb; i++){
+
+//   }
+// } else if (swiperSlide[1].classList.contains(show[6])){
+//   for (i = 1;i <= numb; i++){
+
+//   }
+// }
